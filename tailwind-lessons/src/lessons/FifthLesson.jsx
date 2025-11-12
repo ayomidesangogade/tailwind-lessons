322790db7061e7281b1b9ad88a99d6924bade06a
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-11-12 22:50:00 +0100

    Refactor Fifth and Sixth lesson components for improved layout and navigation

--- tailwind-lessons/src/lessons/FifthLesson.jsx
+++ tailwind-lessons/src/lessons/FifthLesson.jsx
@@ -7,32 +7,38 @@ function FifthLesson() {
 
     return (
         <>
-            <div className="border-blue-400 border-2 p-4 m-4 flex h-64 justify-center">
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
+            <div className="flex justify-between m-4">
+                <Link className="border p-4 bg-green-400 hover:bg-green-600 duration-500 rounded-md text-white" to="/fourth-lesson">Previous lesson</Link>
+                <button onClick={() => setShowPractice(prev => !prev)} className="border border-green-400 px-8 py-4 rounded-md hover:bg-green-400 hover:text-white duration-500">{showPractice ? "Show lesson" : "Show practice"}</button><Link to="/sixth-lesson" className="border p-4 bg-red-400 hover:bg-red-600 duration-500 rounded-md text-white">Next Lesson</Link>
             </div>
-            <div className="border-red-400 border-2 p-4 m-4 flex h-64 justify-between">
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-            </div>
-            <div className="border-2 border-green-400 p-4 m-4 flex h-64 justify-center items-center">
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-            </div>
-            <div className="border-2 border-purple-400 p-4 m-4 flex h-64 justify-around items-end">
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-            </div>
-            <div className="border-2 border-orange-400 p-4 m-4 flex h-64 justify-between items-center">
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-                <Box padding={"p-12"}/>
-            </div>
-            <button onClick={() => setShowPractice(prev => !prev)} className="border border-green-400 px-8 py-4 rounded-md m-4 hover:bg-green-400 hover:text-white">Show practice</button><Link to="/sixth-lesson" className="bg-red-400 px-8 py-4 rounded-md text-white hover:bg-red-500">Sixth Lesson</Link>
+            {!showPractice && <div>
+                <div className="border-blue-400 border-2 p-4 m-4 flex h-64 justify-center">
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                </div>
+                <div className="border-red-400 border-2 p-4 m-4 flex h-64 justify-between">
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                </div>
+                <div className="border-2 border-green-400 p-4 m-4 flex h-64 justify-center items-center">
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                </div>
+                <div className="border-2 border-purple-400 p-4 m-4 flex h-64 justify-around items-end">
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                </div>
+                <div className="border-2 border-orange-400 p-4 m-4 flex h-64 justify-between items-center">
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                    <Box padding={"p-12"}/>
+                </div>
+            </div>}
+            
             {showPractice && <div>
                 <div className="border-blue-400 h-64 border-2 m-4 p-4 rounded-full flex justify-around items-end">
                     <Box padding={"p-12"}/>
--- tailwind-lessons/src/lessons/SixthLesson.jsx
+++ tailwind-lessons/src/lessons/SixthLesson.jsx
@@ -1,21 +1,34 @@
+import {Link} from "react-router-dom";
+
 function SixthLesson() {
     return (
         <div className="flex flex-col items-center m-12">
+            <div className="flex justify-between w-full">
+                <Link className="border p-4 bg-green-400 hover:bg-green-600 duration-500 rounded-md text-white" to="/fifth-lesson">Previous lesson</Link>
+                <Link className="border p-4 bg-red-400 hover:bg-red-600 duration-500 rounded-md text-white" to="">Next lesson</Link>
+            </div>
             {/* <div className="bg-red-400 mb-[15px] w-[180px]">one</div>
             <div className="bg-blue-400 w-[560px]">two</div> */}
-            <div className="flex flex-col mb-16">
+            <div className="flex flex-col mb-16 mt-8">
                 <div className="bg-cyan-600 py-8 px-6 text-cyan-200 rounded-t-lg w-[400px]">One of my most productive days was throwing away 1000 lines of code.</div>
                 <div className="bg-stone-100 p-4 flex flex-col items-center rounded-b-lg border border-stone-200">
                     <div className="text-cyan-700 text-xl mb-2">Ken Thompson</div>
                     <div className="text-stone-400">Designer of Unix Opearting System</div>
                 </div>
             </div>
-            <div className="flex">
-                <div className="bg-blue-500 p-4">
-                    <div>Admiral Grace Hopper</div>
-                    <div>Inventor of Programming Compilers</div>
+            <div className="flex mb-16">
+                <div className="bg-blue-800 p-4 rounded-l-md w-64 border-blue-800">
+                    <div className="text-white text-2xl mb-2">Admiral Grace Hopper</div>
+                    <div className="text-blue-300">Inventor of Programming Compilers</div>
                 </div>
-                <div className="flex items-center border border-stone-400 rounded-r-full p-4">A ship in port is safe, but that's not what ships are built for.</div>
+                <div className="flex items-center border border-stone-400 rounded-r-full p-4 w-80">A ship in port is safe, but that's not what ships are built for.</div>
+            </div>
+            <div className="flex flex-col items-start">
+                <div className="border border-rose-500 p-6 bg-rose-500 text-white rounded-r-md rounded-tl-md text-lg">If you optimise everything, you will always be unhappy.</div>
+                <div className="border-b border-l border-r border-rose-500 p-4 rounded-b-md bg-pink-100">
+                    <div className="text-rose-800 font-bold">Donald Kruth</div>
+                    <div className="text-rose-500">Pioneer of Algorithm Analysis</div>
+                </div>
             </div>
         </div>
     )
